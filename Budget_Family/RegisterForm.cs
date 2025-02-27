ecb1e396cb331c0f38880cc99cd56d6042f7bc32
Thang1807TN <tranthang072k2@gmail.com>
2025-02-27 22:33:25 +0100

    Add files via upload

---
+++ Budget_Family/RegisterForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Budget_Family
+{
+    internal class RegisterForm
+    {
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
