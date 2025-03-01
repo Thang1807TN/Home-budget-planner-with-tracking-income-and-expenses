f03dc4d152dbce8335e5cc8b3e666db185bb1569
Thang1807TN <tranthang072k2@gmail.com>
2025-03-01 10:34:30 +0100

    Add files via upload

---
+++ ManageAccount.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Budget_Family
+{
+    partial class ManageAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnBlock = new System.Windows.Forms.Button();
+            this.btnUnblock = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(80, 125);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(650, 200);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Account Name";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Age";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Password";
+            this.columnHeader4.Width = 100;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Available";
+            this.columnHeader5.Width = 80;
+            // 
+            // btnBlock
+            // 
+            this.btnBlock.AutoSize = true;
+            this.btnBlock.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlock.Location = new System.Drawing.Point(333, 373);
+            this.btnBlock.Name = "btnBlock";
+            this.btnBlock.Size = new System.Drawing.Size(80, 30);
+            this.btnBlock.TabIndex = 1;
+            this.btnBlock.Text = "Block";
+            this.btnBlock.UseVisualStyleBackColor = true;
+            this.btnBlock.Click += new System.EventHandler(this.btnBlock_Click);
+            // 
+            // btnUnblock
+            // 
+            this.btnUnblock.AutoSize = true;
+            this.btnUnblock.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnblock.Location = new System.Drawing.Point(510, 373);
+            this.btnUnblock.Name = "btnUnblock";
+            this.btnUnblock.Size = new System.Drawing.Size(85, 30);
+            this.btnUnblock.TabIndex = 2;
+            this.btnUnblock.Text = "Unblock";
+            this.btnUnblock.UseVisualStyleBackColor = true;
+            this.btnUnblock.Click += new System.EventHandler(this.btnUnblock_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.AutoSize = true;
+            this.btnBack.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(691, 373);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(85, 30);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Manage Account";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.AutoSize = true;
+            this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(89, 373);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 30);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete Account";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // ManageAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnUnblock);
+            this.Controls.Add(this.btnBlock);
+            this.Controls.Add(this.listView1);
+            this.Name = "ManageAccount";
+            this.Text = "ManageAccount";
+            this.Load += new System.EventHandler(this.ManageAccount_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnBlock;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnUnblock;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
---
+++ ManageAccount.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Budget_Family
+{
+    public partial class ManageAccount : Form
+    {
+        public ManageAccount()
+        {
+            InitializeComponent();
+        }
+
+        string connection = @"Data Source=LAPTOP-A08PHTR5\SQLEXPRESS;Initial Catalog=BudgetFamily;Integrated Security=True;Encrypt=False";
+        string sql;
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private void ManageAccount_Load(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        public void ShowData()
+        {
+            listView1.Items.Clear();
+            conn = new SqlConnection(connection);
+            conn.Open();
+            sql = @"SELECT AccountID, Name,Age,Password, Available FROM Account WHERE Authoriza<>'Admin'";
+            cmd = new SqlCommand(sql, conn);
+            dr = cmd.ExecuteReader();
+            int i = 0;
+            while (dr.Read())
+            {
+                listView1.Items.Add(dr[0].ToString());
+                listView1.Items[i].SubItems.Add(dr[1].ToString());
+                listView1.Items[i].SubItems.Add(dr[2].ToString());
+                listView1.Items[i].SubItems.Add(dr[3].ToString());
+                listView1.Items[i].SubItems.Add(dr[4].ToString());
+                i++;
+            }
+            conn.Close();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBlock_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedAccountID = listView1.SelectedItems[0].SubItems[0].Text;
+
+                using (SqlConnection conn = new SqlConnection(connection))
+                {
+                    conn.Open();
+                    string sql = "UPDATE Account SET Available = 'NotActive' WHERE AccountID = @AccountID";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@AccountID", selectedAccountID);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Account has been blocked!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ShowData();
+            }
+            else
+            {
+                MessageBox.Show("Please select an account!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnUnblock_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedAccountID = listView1.SelectedItems[0].SubItems[0].Text;
+                string currentStatus = listView1.SelectedItems[0].SubItems[4].Text;
+                string currentName = listView1.SelectedItems[0].SubItems[1].Text;
+
+                if (currentStatus == "NotActive")
+                {
+                    using (SqlConnection conn = new SqlConnection(connection))
+                    {
+                        conn.Open();
+                        string sql = "UPDATE Account SET Available = 'Active' WHERE AccountID = @AccountID";
+                        using (SqlCommand cmd = new SqlCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@AccountID", selectedAccountID);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    MessageBox.Show("Account has been unblocked!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ShowData();
+                }
+                else
+                {
+                    MessageBox.Show("This account is already active!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an account!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                string selectedAccountID = listView1.SelectedItems[0].SubItems[0].Text;
+
+                using (SqlConnection conn = new SqlConnection(connection))
+                {
+                    conn.Open();
+                    string sql = "DELETE FROM Account WHERE AccountID = @AccountID";
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@AccountID", selectedAccountID);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                MessageBox.Show("Account has been delete!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ShowData();
+            }
+            else
+            {
+                MessageBox.Show("Please select an account!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            App app = new App();
+            app.Show();
+        }
+    }
+}
